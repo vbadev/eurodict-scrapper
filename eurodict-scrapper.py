ce9406649cc97bba8d3d3be8c856f08565bd91cf
vbadev <vbadev@gmail.com>
2014-05-14 07:36:44 +0300

    More robust code for url construction.
    Print usage if started without parameters.

--- eurodict-scrapper.py
+++ eurodict-scrapper.py
@@ -1,31 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from BeautifulSoup import BeautifulSoup
 import sys
+import urllib
 import urllib2
 import cookielib
+from BeautifulSoup import BeautifulSoup
 
-urlopen = urllib2.urlopen
-Request = urllib2.Request
-cj = cookielib.LWPCookieJar()
+# if we use cookielib then we get the HTTPCookieProcessor and install the opener in urllib2
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.LWPCookieJar()))
+urllib2.install_opener(opener)
 
-# Now we need to get our Cookie Jar installed in the opener for fetching URLs
-if cookielib is not None:
-    # if we use cookielib then we get the HTTPCookieProcessor and install the opener in urllib2
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-    urllib2.install_opener(opener)
+if len(sys.argv) < 2:
+	import os.path
+	print "Usage: ", os.path.basename(sys.argv[0]), " <word> [dictionary]"
+	print "Avaiable dictionaries:"
+	response = urllib2.urlopen("http://www.eurodict.koralsoft.com")
+	soup = BeautifulSoup(response.read())
+	select = soup.find('select', attrs={'name': 'diction'})
+	for o in select.contents:
+		print '    ', o['value'], ': ', o.contents[0]
+	sys.exit()
 
-url = (u'http://www.eurodict.com/search.php?word=' + sys.argv[1].decode('utf-8') + u'&go=%D0%9F%D1%80%D0%B5%D0%B2%D0%BE%D0%B4&ok=1&diction=ed_en_bg').encode('utf-8')
+word = sys.argv[1]
+diction = 'ed_en_bg'
+if len(sys.argv) > 2:
+	diction = sys.argv[2]
 
+url = 'http://www.eurodict.com/search.php?' + urllib.urlencode({'word': sys.argv[1], 'go': 'Превод', 'ok': '1', 'diction': diction})
 txdata = None
 txheaders = {'User-agent':'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'}
 
-req = Request(url, txdata, txheaders)
-response = urlopen(req)
+req = urllib2.Request(url, txdata, txheaders)
+response = urllib2.urlopen(req)
 html = response.read()
 
-soup = BeautifulSoup(html)
+soup = BeautifulSoup(html, fromEncoding="utf-8")
 tree = soup.find('td', attrs={'class':'meaning_container'})
 
 deltags = tree.findAll('div')
@@ -36,7 +46,7 @@ deltags.extend(tree.findAll('h5'))
 for t in deltags:
 	t.extract()
 
-head = u'''<html>\n<head>
+head = '''<html>\n<head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <style>
 .wordtitle{
@@ -55,4 +65,4 @@ head = u'''<html>\n<head>
 output = str(tree)
 lidx = output.find('<span class="wordtitle">')
 ridx = output.find('<span class="adcont">')
-print head, output[lidx:ridx], u'\n</body>\n</html>'
+print head, output[lidx:ridx], '\n</body>\n</html>'
